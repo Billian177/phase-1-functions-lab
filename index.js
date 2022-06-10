be53d4850afc6db4b2ef0cae52308dbb7bc00687
Billian177 <61032906+Billian177@users.noreply.github.com>
2022-06-10 23:25:54 +0300

    Update index.js
    Assignment, refused to update on git so I had to edit it in here

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const street=43;
+let blocks, feet, distanceT, fare;
+function distanceFromHqInBlocks(street){
+    return blocks= Math.abs(street-42);
+}
+
+function distanceFromHqInFeet(street) {
+    return feet= distanceFromHqInBlocks(street)*264;
+}
+
+function distanceTravelledInFeet(street, street1) {
+    return distanceT= Math.abs(street-street1)*264;
+}
+
+function calculatesFarePrice(street, street1) {
+    let distanceT= distanceTravelledInFeet(street, street1)
+
+    if (distanceT <=400) {
+        return fare= 0;
+    } else if (distanceT>400 && distanceT<= 2000) {
+        return fare= ((distanceT-400)*2)/100;
+    } else if(distanceT> 2000 && distanceT<= 2500){
+        return fare= 25;
+    } else{
+        return 'cannot travel that far'
+    }
+}
